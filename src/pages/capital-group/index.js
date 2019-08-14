a9411f9210ad2ad79d83a193d8ee9867cb28527c
akshaikrish <akshaikrishna@gmail.com>
2019-08-14 12:36:13 +0530

    Capital Group page

---
+++ src/pages/capital-group/index.js
@@ -0,0 +1,4 @@
+import "bootstrap/js/src/index";
+import $ from "jquery";
+
+$(() => console.log("Capital Group page loaded!!!"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require("path");
-const homepageJS = path.resolve(__dirname, "src/pages/homepage");
-const stickynavpageJS = path.resolve(__dirname, "src/pages/stickynavpage");
-const accordionpageJS = path.resolve(__dirname, "src/pages/accordionpage");
+// const homepageJS = path.resolve(__dirname, "src/pages/homepage");
+// const stickynavpageJS = path.resolve(__dirname, "src/pages/stickynavpage");
+// const accordionpageJS = path.resolve(__dirname, "src/pages/accordionpage");
+const capitalpageJS = path.resolve(__dirname, "src/pages/capital-group");
 
 module.exports = {
   entry: {
-    homepage: homepageJS,
-    stickynavpage: stickynavpageJS,
-    accordionpage: accordionpageJS
+    capitalpage : capitalpageJS
+    // homepage: homepageJS,
+    // stickynavpage: stickynavpageJS,
+    // accordionpage: accordionpageJS
   },
   output: {
     path: path.resolve(__dirname, "dist"),

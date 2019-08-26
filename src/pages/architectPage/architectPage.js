792f3a03e71e8c7bf30f710b8ab066bffa4c70ac
akshaikrish <akshaikrishna@gmail.com>
2019-08-26 13:33:21 +0530

    architect page first commit here

---
+++ src/pages/architectPage/architectPage.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    /* Carousel */
+    $('.carousel').carousel();
+    
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo").attr('src');
+    
+    /* Remove iframe src attribute on page load to
+    prevent autoplay in background */
+    $("#cartoonVideo").attr('src', '');
+	
+	/* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed */
+    $("#myModal").on('shown.bs.modal', function(){
+        $("#cartoonVideo").attr('src', url);
+    });
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function(){
+        $("#cartoonVideo").attr('src', '');
+    });
+
+    
+});
